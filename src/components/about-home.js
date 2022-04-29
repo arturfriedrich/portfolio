ee2974088c7beb4ec90830ebe489856f08e51b50
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-29 15:06:49 +0200

    Bug: too wide

--- src/components/about-home.js
+++ src/components/about-home.js
@@ -5,21 +5,42 @@ import { Link as ReachRouterLink } from "react-router-dom"
 // styled components
 const Container = styled.div`
     display: flex;
+    flex-direction: column;
     background-color: #2A2438;
     justify-content: space-between;
     align-items: center;
-    height: calc(100vh - 75px);
+    height: 100vh;
     background-color: #2A2438;
 `
 
 const Group = styled.div`
-    width: 500px;
-    height: 350px;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-basis: auto;
+    heigth: 100%;
+    margin-bottom: 110px;
+    gap: 150px;
+`
 
-    &:last-pf-type {
-        background: #DBD8E3;
-        border-radius: 25px;
+const TextBox = styled.div`
+    width: 500px;
+    height: 250px;
+    background: #DBD8E3;
+    border-radius: 25px;
+    padding: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transform: scale(1);
+    transition: 200ms;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+
+    &:hover {
+        transform: scale(1.01);
+        border: 2px solid #FFB703;
     }
 `
 
@@ -28,6 +49,7 @@ const Title = styled.p`
     font-size: 64px;
     line-height: 27px;
     letter-spacing: 0.18em;
+    margin: 0px;
 
     color: #DBD8E3;
 `
@@ -40,8 +62,8 @@ const Text = styled.p`
 `
 
 const Image = styled.img`
-    width: 242px;
-    height: 430px;
+    width: 253.26px;
+    height: 450px;
 `
 
 const Link = styled.a``
@@ -50,13 +72,15 @@ export default function AboutHome() {
     return (
             <Container>
                 <Title>About</Title>
-                <Image src={require("../images/misc/peep-standing.png")} />
                 <Group>
-                    <Text>
-                        My name is Artúr Friedrich, I started learning web developemt  in February of 2021. 
-                        I write programs for 4 years not and I am currently a Computer Science student at the 
-                        University of Győr, and want to get some experience working in the tech industry.
-                    </Text>
+                    <Image src={require("../images/misc/peep-standing.png")} />
+                    <TextBox>
+                        <Text>
+                            My name is Artúr Friedrich, I started learning web developemt  in February of 2021.<br /><br /> 
+                            I write programs for 4 years not and I am currently a Computer Science student at the 
+                            University of Győr, and want to get some experience working in the tech industry.
+                        </Text>
+                    </TextBox>
                 </Group>
             </Container>
     )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,13 @@
 import React from "react"
 
-import { Header, Hero } from "../components"
+import { Header, Hero, AboutHome } from "../components"
 
 export default function Home() {
     return (
         <>
             <Header />
             <Hero />
+            <AboutHome />
         </>
     )
 }
