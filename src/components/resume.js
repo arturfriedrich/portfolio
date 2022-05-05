e30c296e7b072c7fde29441a41d8f577a20465c7
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-05-05 08:33:00 +0200

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -15,46 +15,70 @@ const Container = styled.div`
 `
 
 const Tanulmanyok = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: tanulmanyok;
+    height: 150px;
 `
 
 const Minositesek = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: minositesek;
+    height: 150px;
 `
 
 const Nev = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: nev;
+    height: 300px;
 `
 
 const Elerhetosegek = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: elerhetosegek;
+    height: 300px;
 `
 
 const Bemutatkozas = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: bemutatkozas;
+    height: 300px;
 `
 
 const Technologiak = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: technologiak;
+    height: 300px;
 `
 
 const Hobbi = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: hobbi;
+    height: 300px;
 `
 
 const Tapasztalat = styled.div`
+    background: #352F44;
+    border-radius: 20px;
     grid-area: tapasztalat;
+    height: 300px;
 `
 
 export default function Resume() {
     return (
         <Container>
-            <Tanulmanyok></Tanulmanyok>
-            <Minositesek></Minositesek>
             <Nev></Nev>
             <Elerhetosegek></Elerhetosegek>
             <Bemutatkozas></Bemutatkozas>
             <Technologiak></Technologiak>
+            <Tanulmanyok></Tanulmanyok>
+            <Minositesek></Minositesek>
             <Hobbi></Hobbi>
             <Tapasztalat></Tapasztalat>
         </Container>
